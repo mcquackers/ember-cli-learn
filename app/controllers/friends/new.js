3cf5d488190feb4507de9d83bc57a73c7d1a350a
Brian McQueen <brian.d.mcqueen@gmail.com>
2015-03-28 12:23:54 -0400

    Long overdue commit

---
+++ app/controllers/friends/new.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+               "model.email",
+               "model.firstName",
+               "model.lastName",
+               "model.twitter",
+               function() {
+                 return !Ember.isEmpty(this.get("model.email"))&&
+                   !Ember.isEmpty(this.get("model.firstName"))&&
+                   !Ember.isEmpty(this.get("model.lastName"))&&
+                   !Ember.isEmpty(this.get("model.twitter"));
+               }
+               ),
+  actions: {
+    save: function() {
+      console.log("+- save action in friends new controller");
+      return true;
+    },
+    cancel: function() {
+      console.log("+- cancel action in friends new controler");
+      return true;
+    }
+  }
+});
